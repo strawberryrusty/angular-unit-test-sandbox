f4393f180ff84a33f0cb0d6726e8d28936159008
Prashanth Mohan <prashmohanuon@gmail.com>
2022-09-29 13:25:36 +0200

    form unit tests

--- src/app/Basic Unit Tests/04-forms/todo-form.component.spec.ts
+++ src/app/Basic Unit Tests/04-forms/todo-form.component.spec.ts
@@ -1,22 +1,27 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { TodoFormComponent } from './todo-form.component';
 import { FormBuilder } from '@angular/forms';
 
 describe('TodoFormComponent', () => {
-  let component: TodoFormComponent; 
+  var component: TodoFormComponent;
 
   beforeEach(() => {
-    // have to pass in all dependencies to contructor
-    component =  new TodoFormComponent(new FormBuilder());
+   component = new TodoFormComponent(new FormBuilder());
+
+
   });
 
   it('should create a form with 2 controls', () => {
-    expect(component.form.contains('name')).toBeTruthy();
-    expect(component.form.contains('email')).toBeTruthy();
+    expect(component.form.contains('name')).toBeTruthy;
+    expect(component.form.contains('email')).toBeTruthy;
   });
 
   it('should make the name control required', () => {
+    //with this we can get a particular form control inside this form group
     let control = component.form.get('name');
+
+    //set the value of that form control to be nothing, this way we can expect it to be falsey
     control.setValue('');
-    expect(control.valid).toBeFalsy();
+
+    expect(control.valid).toBe(false);
   });
 });
