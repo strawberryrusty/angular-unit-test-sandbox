d07d77157b8824fe50a16c98637843db9a95a810
Prashanth Mohan <prashmohanuon@gmail.com>
2022-11-15 16:33:38 +0100

    9.working with spies

--- src/app/Basic Unit Tests/06-services/todos.component.spec.ts
+++ src/app/Basic Unit Tests/06-services/todos.component.spec.ts
@@ -2,6 +2,7 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
 
 
 describe('TodosComponent', () => {
@@ -18,16 +19,39 @@ describe('TodosComponent', () => {
     //first arg is the object (service), second is the method in the object 'getTodos'.
     //getTodos is a method with no params which returns an observable
     //callFake allows us to change the implementation of this method.
-    let todos = [1,2,3];
+
+    //arrange
+
+    let faketodos = [1,2,3];
 
     spyOn(service, 'getTodos').and.callFake(() => {   //the whole point of this is the replicate the TodoService and the 'getTodos'
-      return Observable.from([todos])               //method within it without actually using the service
-    });
+      return Observable.from([faketodos])               //method within it without actually using the service
+    });                                               //this method has no params and returns an observable (look at method in the service)
 
+    //act
     component.ngOnInit();
 
     //assertion
-
-    expect(component.todos).toBe(todos);
+    expect(component.todos).toBe(faketodos);
   });
+
+  //so here, we setting the fake todos that we get from our service, and seeing if the component, sets the "todo" array to
+  //the fake todo
+
+//   it('should call the server to save the changes when a new todo item is added', () => {
+//     let spy = spyOn(service, 'add').and.callFake(t => {
+//       return Observable.empty;
+//     });
+
+//   it('should add the new todo returned from the server', () => {
+//     let todo = {id: 1};
+//     let spy = spyOn(service, 'add').and.callFake(t => {
+//       return Observable.from([todo]);
+//     });
+
+//     component.add();
+
+//     expect(spy).toHaveBeenCalled();
+//   });
+// }
 });
